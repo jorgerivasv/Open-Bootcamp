0984d042fbbad289ad529e1e152aff49e8d0940a
jorgerivasv <81697406+jorgerivasv@users.noreply.github.com>
2023-02-04 19:30:04 -0300

    Add files via upload

---
+++ JavaBasico/src/com/company/Variables.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int variable1 = 1;
+        long variable2 = 22222;
+        double variable3 = 1.1;
+        boolean variable4 = true;
+        String variable5 = "hola mundo";
+
+        System.out.println("variable1: " + variable1);
+        System.out.println("variable2: " + variable2);
+        System.out.println("variable3: " + variable3);
+        System.out.println("variable4: " + variable4);
+        System.out.println("variable5: " + variable5);
+    }
+}
